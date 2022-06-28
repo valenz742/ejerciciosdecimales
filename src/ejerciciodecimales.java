27c0c7e7038f18af46c3e3d7d18fd712cf2211f5
Jose Valenzuela <valenz743@gmail.com>
2022-06-28 13:55:16 -0500

    En este primer commit, definimos variables tipo float

---
+++ src/ejerciciodecimales.java
@@ -0,0 +1,11 @@
+class ejerciciosdecimales {
+    public static void main(String[] args) {
+        float a = 10.5f;
+        float b = 1.5f;
+        float c = a + b;
+
+        System.out.println(c);
+
+
+    }
+}
